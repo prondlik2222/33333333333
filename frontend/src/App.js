74a4ca9473b6856d9ac970f658baeee598555928
rk28284 <112754760+rk28284@users.noreply.github.com>
2024-08-21 18:36:42 +0530

    Completed Tapme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,31 +5,30 @@ import './App.css';
 function App() {
   const [count, setCount] =useState(0)
   return (
-    <div id="App" className="container mx-auto mt-4 p-4 bg-gradient-dark">
+    <div id="App" className="container mx-auto mt-0 p-2 bg-gradient-dark">
       <h1 className="text-white display-4 fw-bold mb-2">
         TapSwap
       </h1>
 
-      <div className="row gx-4 mb-4">
+      <div className="row gx-4 mb-4 shadow-lg py-4 px-4">
         <div className="col-3 text-center">
-          <span>🟡</span>
+          <span>💰</span>
           <span className="ms-2 text-white">{count}</span>
         </div>
         <div className="col-3 text-center">
-          <span>🔷</span>
+          <span>📈</span>
           <span className="ms-2 text-white">0/h</span>
         </div>
         <div className="col-3 text-center">
-          <span>🌟</span>
+          <span>🏆</span>
           <span className="ms-2 text-white">0</span>
         </div>
         <div className="col-3 text-center">
-          <span>🟣</span>
+          <span>🔥</span>
           <span className="ms-2 text-white">0</span>
         </div>
       </div>
          
-      {/* Container for the image */}
       <div className="d-flex justify-content-center mb-4">
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4EGUMaDiGjK_20AqlIuL338KRk-ksyis33Q&s"
@@ -40,21 +39,19 @@ function App() {
       </div>
       <br />
       <br />
-      <br />
-      <br />
-      <br />
-      <br />
+     
+     
       <footer className="footer bg-light p-4 text-center">
         <div className="row gx-4">
           <div className="col-2">
             <button className="btn btn-purple btn-sm w-100">
-              <span className="text-2xl">🟡</span>
+              <span className="text-2xl">💸</span>
               <span className="text-sm mt-1">Earn</span>
             </button>
           </div>
           <div className="col-2">
             <button className="btn btn-purple btn-sm w-100">
-              <span className="text-2xl">✔️</span>
+              <span className="text-2xl">📝</span>
               <span className="text-sm mt-1">Tasks</span>
             </button>
           </div>
@@ -72,7 +69,7 @@ function App() {
           </div>
           <div className="col-2">
             <button className="btn btn-purple btn-sm w-100">
-              <span className="text-2xl">😎</span>
+              <span className="text-2xl">👥</span>
               <span className="text-sm mt-1">Account</span>
             </button>
           </div>
